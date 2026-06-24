0db8ed8850b09690c33c1687c4efdf3617dc5341
Flávio Bezerra Pereira <flaviobp@gmail.com>
2026-06-24 11:17:11 -0300

    Atualiza board-dezesseis para listar 16 jogos separados (issue #9)

--- src/components/board-dezesseis.component.js
+++ src/components/board-dezesseis.component.js
@@ -2,10 +2,41 @@ import React, { Component } from "react";
 import GrupoPartida from "./grupopartida.component";
 
 function BetDezesseis()  {
-    return (    
-        <div class="row justify-content-center">
-            <GrupoPartida name="Dezesseis avos de Final" stage="DEZESSEISFINAL"/>
+    return (
+      <div>
+        <div class="row">
+          <GrupoPartida name="Jogo 01 (16-avos)" stage="16JOGO01"/>
+          <GrupoPartida name="Jogo 02 (16-avos)" stage="16JOGO02"/>
         </div>
+        <div class="row">
+          <GrupoPartida name="Jogo 03 (16-avos)" stage="16JOGO03"/>
+          <GrupoPartida name="Jogo 04 (16-avos)" stage="16JOGO04"/>
+        </div>
+        <div class="row">
+          <GrupoPartida name="Jogo 05 (16-avos)" stage="16JOGO05"/>
+          <GrupoPartida name="Jogo 06 (16-avos)" stage="16JOGO06"/>
+        </div>
+        <div class="row">
+          <GrupoPartida name="Jogo 07 (16-avos)" stage="16JOGO07"/>
+          <GrupoPartida name="Jogo 08 (16-avos)" stage="16JOGO08"/>
+        </div>
+        <div class="row">
+          <GrupoPartida name="Jogo 09 (16-avos)" stage="16JOGO09"/>
+          <GrupoPartida name="Jogo 10 (16-avos)" stage="16JOGO10"/>
+        </div>
+        <div class="row">
+          <GrupoPartida name="Jogo 11 (16-avos)" stage="16JOGO11"/>
+          <GrupoPartida name="Jogo 12 (16-avos)" stage="16JOGO12"/>
+        </div>
+        <div class="row">
+          <GrupoPartida name="Jogo 13 (16-avos)" stage="16JOGO13"/>
+          <GrupoPartida name="Jogo 14 (16-avos)" stage="16JOGO14"/>
+        </div>
+        <div class="row">
+          <GrupoPartida name="Jogo 15 (16-avos)" stage="16JOGO15"/>
+          <GrupoPartida name="Jogo 16 (16-avos)" stage="16JOGO16"/>
+        </div>		
+      </div>
     );
 }
 
